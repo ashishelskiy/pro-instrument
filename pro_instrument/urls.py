c6f98b8b51f95e3343d39b082279ca96407841f5
ashishelskiy@gmail.com <ashishelskiy@gmail.com>
2026-09-23 18:13:52 +0300

    Rename admin URL to /panel/

--- pro_instrument/urls.py
+++ pro_instrument/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('panel/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
